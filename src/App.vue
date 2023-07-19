49d21383e45f545b07c327e20dd347b3f2a91913
zhangboMsb <zhangbo819@163.com>
2023-07-19 10:34:51 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
+      <router-link to="/china">China</router-link>
     </nav> -->
-    <router-link to="/china">China</router-link>
     <router-view />
   </div>
 </template>
