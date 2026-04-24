da0258f4491d6b2fb321e322f5f8892b3517168e
zzb2023 <297234792@qq.com>
2026-04-24 11:06:02 +0900

    feat: use dist

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
     // },
   },
   build: {
-    outDir: "docs", // 输出目录名
+    outDir: "dist", // 输出目录名
     // minify: "terser", // 压缩方式
     terserOptions: {
       // keep_fnames: true, // 打包后的仍保持原函数名，debug 用
