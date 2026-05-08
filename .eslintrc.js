e1f9f2499327b309e4f0017d196e865a9e9e2540
zhangbo-tal <297234792@qq.com>
2026-05-08 11:01:13 +0900

    build: prettier eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-  parser: "vue-eslint-parser",
+  parser: 'vue-eslint-parser',
 
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
+    sourceType: 'module',
   },
 
   extends: [
-    "plugin:vue/vue3-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+
+    // ⭐ 必须放最后
+    'plugin:prettier/recommended',
   ],
 
   rules: {
-    // override/add rules settings here, such as:
+    // ❗ 不要写任何格式规则（quotes / semi 等）
   },
 };
