68e8809896d63e0365d077e58f0882424b589695
zzb2023 <297234792@qq.com>
2026-05-09 17:56:56 +0900

    style: dialog

--- src/views/Bazi/components/BaziModal.vue
+++ src/views/Bazi/components/BaziModal.vue
@@ -1,10 +1,10 @@
 <template>
   <van-popup v-model:show="store.visible" round closeable class="bazi-popup">
     <header>
-      <h2>{{ store.title }}</h2>
+      <h2 class="title">{{ store.title }}</h2>
     </header>
     <section>
-      <p>{{ store.text }}</p>
+      <p class="text">{{ store.text }}</p>
       <!-- 这里放你的八字内容 -->
       <!-- <slot :payload="store.payload">
             <pre>{{ store.payload }}</pre>
@@ -24,6 +24,14 @@ const store = useBaziModalStore();
   padding: 0 2em 1em;
   min-width: 50%;
   max-width: 90%;
+  max-height: 90%;
   text-align: left;
+}
+.title {
+  margin-bottom: 0;
+}
+.text {
+  margin-top: 0;
+  white-space: pre-line;
 }
 </style>
