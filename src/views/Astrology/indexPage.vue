710796b34b25e2b0a6ba03905dd31a5e33da81b6
zzb2023 <297234792@qq.com>
2026-05-16 10:21:19 +0900

    refactor: workspace

--- src/views/Astrology/indexPage.vue
+++ src/views/Astrology/indexPage.vue
@@ -13,37 +13,6 @@
         <template #title> <h2>操作栏</h2></template>
         <astro-operation v-model:time="time" />
       </van-collapse-item>
-      <!-- 格局 -->
-      <van-collapse-item v-if="patternData.length" name="1">
-        <template #title> <h2 style="color: #f00">格局</h2></template>
-        <van-cell-group inset>
-          <van-cell v-for="item in patternData" :key="item.type + item.planets" :label="item.type">
-            <template #title>
-              <p>
-                <span :style="{ color: patternMap[item.type].color }"
-                  >{{ patternMap[item.type].text }}{{ patternData.length <= 1 ? '格局' : '' }}</span
-                >
-              </p>
-            </template>
-            <p :style="{ color: patternMap[item.type].color }">{{ patternMap[item.type].desc }}</p>
-            <p class="value">
-              参与行星
-              <span
-                v-for="(p, index) in item.planets"
-                :key="p"
-                :style="{
-                  color: planentsMap[p].color,
-                  display: 'inline-block',
-                  marginLeft: '2px',
-                }"
-              >
-                <template v-if="index !== 0">|</template>
-                {{ planentsMap[p].name }}
-              </span>
-            </p>
-          </van-cell>
-        </van-cell-group>
-      </van-collapse-item>
 
       <!-- 参数详情列表 -->
       <van-collapse-item name="2">
@@ -66,62 +35,6 @@
           </van-cell>
         </van-cell-group>
       </van-collapse-item>
-
-      <!-- 相位 -->
-      <van-collapse-item name="3">
-        <template #title> <h2>主要相位</h2></template>
-        <van-cell-group inset>
-          <van-cell
-            v-for="item in aspectData"
-            :key="item.between"
-            :label="item.between.join(' - ')"
-          >
-            <template #title>
-              <p>
-                <span :style="{ color: planentsMap[item.between[0]].color }">{{
-                  planentsMap[item.between[0]].name
-                }}</span>
-                -
-                <span :style="{ color: planentsMap[item.between[1]].color }">{{
-                  planentsMap[item.between[1]].name
-                }}</span>
-              </p>
-            </template>
-            <p class="value" :style="{ color: aspectPosition.map[item.type].color }">
-              {{ item.type }} {{ aspectPosition.map[item.type].name }}
-            </p>
-            <p class="value">
-              开始时间 {{ item.window.start.toLocaleDateString() }}
-              {{ item.window.start.getHours() }}:{{ item.window.start.getMinutes() }}
-            </p>
-            <p class="value">
-              力量最强 {{ item.window.exact.toLocaleDateString() }}
-              {{ item.window.exact.getHours() }}:{{ item.window.exact.getMinutes() }}
-            </p>
-            <p class="value">
-              结束时间 {{ item.window.end.toLocaleDateString() }}
-              {{ item.window.end.getHours() }}:{{ item.window.end.getMinutes() }}
-            </p>
-            <p class="value">计算耗时 {{ item.window._t }} ms</p>
-            <!-- <p
-          class="value"
-          :style="{
-            fontWeight: item.strength === 'strong' ? 'bold' : 'normal',
-          }"
-        >
-          {{ item.strength }} ({{ item.orb }}°)
-        </p> -->
-          </van-cell>
-        </van-cell-group>
-      </van-collapse-item>
-
-      <!-- 八字 临时放这里 -->
-      <van-collapse-item name="4">
-        <template #title>
-          <h2>八字</h2>
-        </template>
-        <BaziPan :time="time" />
-      </van-collapse-item>
     </van-collapse>
   </div>
 </template>
@@ -129,12 +42,10 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import router from '@/router';
-import { getAllPlanets, aspectPosition } from '@/utils/astro/planets';
-import { map12, patternMap, planentsMap } from '@/utils/astro/astroUI';
-import { AspectPatternEngine } from '@/utils/astro/aspectPattern';
-import AstroOperation from './components/AstroOperation.vue';
-import AstroRoundPlate from './components/AstroRoundPlate.vue';
-import BaziPan from '../Bazi/components/BaziPan.vue';
+import { getAllPlanets } from '@/utils/astro/planets';
+import { map12, planentsMap } from '@/utils/astro/astroUI';
+import AstroOperation from '@/views/Workspace/Astrology/components/AstroOperation.vue';
+import AstroRoundPlate from '@/views/Workspace/Astrology/components/AstroRoundPlate.vue';
 
 // const time = ref(new Date('2013-07-29T04:30:00Z'));
 // const time = ref(new Date('2010-08-07T00:00:00Z'));
@@ -151,28 +62,6 @@ const onClickRight = () => {
     query: { time: time.value.getTime() },
   });
 };
-
-// 相位
-const aspectData = computed(() => {
-  return aspectPosition.getData(data.value).map((i) => {
-    const r = aspectPosition.findAspectWindow(time.value, i.between[0], i.between[1], i.type);
-    return {
-      ...i,
-      window: r,
-    };
-  });
-});
-
-// 格局
-const patternData = computed(() => {
-  const engine = new AspectPatternEngine(aspectData.value, data.value);
-  const { patterns } = engine.detectAll();
-
-  // if (patterns.length) {
-  //   console.log(patterns);
-  // }
-  return patterns;
-});
 </script>
 
 <style lang="scss" scoped>
--- src/views/Workspace/indexPage.vue
+++ src/views/Workspace/indexPage.vue
@@ -1,10 +1,16 @@
 <template>
   <van-nav-bar title="标题" left-text="返回" left-arrow @click-left="onClickLeft" />
 
-  <van-tabs v-model:active="activeTop" swipeable animated sticky>
-    <van-tab title="标签 1">内容 1</van-tab>
-    <van-tab title="标签 2">内容 2</van-tab>
-  </van-tabs>
+  <template v-if="activeBottom === 0">
+    <!-- <van-tabs v-model:active="activeTop" swipeable animated sticky>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+    </van-tabs> -->
+    <Astrology :time="time" />
+  </template>
+  <template v-else-if="activeBottom === 1">
+    <bazi-pan :time="time" />
+  </template>
 
   <van-tabbar v-model="activeBottom" placeholder>
     <van-tabbar-item icon="home-o">星盘</van-tabbar-item>
@@ -15,8 +21,10 @@
 <script lang="ts" setup>
 import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
+import Astrology from './Astrology/indexPage.vue';
+import BaziPan from './Bazi/components/BaziPan.vue';
 
-const activeTop = ref(0);
+// const activeTop = ref(0);
 const activeBottom = ref(0);
 
 const time = ref(new Date());
@@ -25,9 +33,12 @@ const route = useRoute();
 watch(
   () => route.query.time,
   (val) => {
-    if (typeof val === 'string' && val !== String(time.value.getTime())) {
-      time.value = new Date(val);
+    if (Number(val) !== time.value.getTime()) {
+      time.value = new Date(Number(val));
     }
+  },
+  {
+    immediate: true,
   }
 );
 
